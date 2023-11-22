8e13209cae69d8d89bca1713c5a2e67f99fa4fac
Yerlin ROJAS <yerlinrojas808@gmail.com>
2023-11-22 17:05:05 -0300

    carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import { initFlowbite } from 'flowbite';
 
 // https://astro.build/config
 export default defineConfig({
