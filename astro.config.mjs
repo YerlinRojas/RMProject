7dc65e56e33c9c4eb1ada15ef01dc63cdfe5361b
JM-Dev <jtogni.developer@gmail.com>
2023-12-12 01:08:50 -0300

    alert en formulario y pagina de preg frecuentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import { initFlowbite } from 'flowbite';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
